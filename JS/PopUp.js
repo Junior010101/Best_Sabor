2bbf35a39fb33f79cb9abc639e2e570a97d5e2f7
Junior010101 <marcondesjunior2209@gmail.com>
2024-08-05 01:05:51 -0300

    Adição do PopUp

---
+++ JS/PopUp.js
@@ -0,0 +1,34 @@
+const fechar = document.getElementById("fechar");
+const popup = document.querySelector("dialog");
+const butao = document.getElementById("botao");
+
+butao.onclick = function () {
+    popup.showModal()
+
+    setTimeout( function(){
+        Carregamento_completo()
+    }, 4600)    
+}
+fechar.onclick = function () {
+    popup.close(
+        Reset()
+    )
+}
+document.addEventListener("keydown", event => {
+    if(event.key.startsWith("Escape")){
+        return Reset()
+    }
+});
+//Loading
+function Carregamento_completo(){
+    document.querySelector(".tela").classList.remove("carregando")
+    document.querySelector(".span").classList.add("pontos")
+    document.querySelector(".spano").classList.add("pontos")
+    document.querySelector(".spana").classList.add("pontos")
+}
+function Reset(){
+    document.querySelector(".tela").classList.add("carregando")
+    document.querySelector(".span").classList.remove("pontos")
+    document.querySelector(".spano").classList.remove("pontos")
+    document.querySelector(".spana").classList.remove("pontos")
+}
